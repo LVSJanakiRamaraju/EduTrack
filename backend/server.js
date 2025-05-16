4b23b988cb6e835c71b45348430e048bd638569d
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-05-16 10:41:34 +0530

    Added deploy link in cors

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,25 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+
+const allowedOrigins = [
+  'https://edu-track-ten.vercel.app/',
+  'http://localhost:5173',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 
 const Student = require('./models/Student');
